6b48b2a305d2c6ec9235776f272356fe53da2769
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-11-02 21:05:22 +0600

    first commit

---
+++ pages/components/GameOverlay.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from 'next/image'
+import TryAgainLogo from "../../public/try-again.gif";
+
+const GameOverlay = ({ onRestart, board }) => {
+  if (board.hasWon()) {
+    return <div className="tile2048"></div>;
+  } else if (board.hasLost()) {
+    return (
+      <div className="gameOver" onClick={onRestart}>
+        <Image
+          src={TryAgainLogo}
+          alt="Try Again"
+          style={{
+            width: "100%",
+            height: "100%",
+            cursor: "pointer",
+          }}
+        />
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default GameOverlay;
